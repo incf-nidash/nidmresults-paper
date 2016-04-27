50dfc3cb16a3bb4f6c5952218198ba0691501fa5
Alex Bowring <A.Bowring@warwick.ac.uk>
2016-04-27 11:45:11 +0100

    MKDAscript file updated with comments where users must specify options, and results file added for analysis on the pain.csv database

--- MKDAscript.m
+++ MKDAscript.m
@@ -1,19 +1,19 @@
 
-COI_directory = 'pain_subset_contrast_test'; %- This is the directory where data of the chosen contrast will be stored
-iterations = 300; %- Iterations of Monte Carlo method
-dbname ='pain_subset.txt'; %- Name of the database (must be a .txt file)
+COI_directory = 'pain_contrast'; %- This is the directory where data of the chosen contrast will be stored
+iterations = 15000; %- Iterations of Monte Carlo method
+dbname ='pain.csv'; %- Name of the database (must be a .txt file)
 
-read_database;
+read_database; %- This genereates a .mat file named by the user containing the database information
 DB=Meta_Setup (DB, 10);
 
 mkdir(COI_directory);
 cd(COI_directory);
 
-DB=Meta_Select_Contrasts(DB);
-save SETUP DB
+DB=Meta_Select_Contrasts(DB); %- During this stage the specifies which variables (regressors) will be used during the analysis
+save SETUP DB  
 load SETUP.mat
 
-MC_Setup = Meta_Activation_FWE('setup', DB); %- This should create a MC_Info.mat file. This may not happen if this file exists in another directory.
+MC_Setup = Meta_Activation_FWE('setup', DB); %- Here the user must specify which contrasts will be computed in the analysis. An MC_Info.mat file is created. Note, this may not happen if this file exists in another directory.
 load MC_Info.mat
 
 Meta_Activation_FWE('mc',iterations);
